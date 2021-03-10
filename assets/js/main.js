7311d7e62a9789afec5456ce93ee4faefe985de0
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-10 14:21:34 +0100

    Divisione problemi

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//Grandi problemi
+//Trovare il menù hamburger nel HTML
+//elemento hamburger a href con dentro i class="fas fa-bars"
+//Contenuto menù hamburger div class="hamburger-menu";
+
+//Divisione in piccoli problemi
+//Di partenza il div hamburger menù non deve vedersi
+
+// Al click sull'elemento hamburger il div hamburger menu deve tirarsi scendere
+
+//Cliccando nuovamente il div hamburger deve risalire
