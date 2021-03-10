d48b825150247a19211a383a5d4a5b708e12a573
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-10 17:37:51 +0100

    Esercizio completato  - aggiunta terza soluzione

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-/* --- SOLUZIONE 1 ---
+/*
+ // --- SOLUZIONE 1 ---
 
 //Dichiaro la variabile e inserisco dentro il
 //collegamento all'a contente il simbolo del menù
@@ -21,10 +22,10 @@ hamburgerMenuChiusura.click(function () {
   var listaMenu = $('.hamburger-menu');
   //Diciamo alla lista contente il menù che deve nascondersi
   listaMenu.hide();
-})
-*/
+});
+/*
 
-
+/*
 // --- SOLUZIONE 2 ---
 
 //Dichiaro la variabile e inserisco dentro il
@@ -48,4 +49,30 @@ hamburgerMenuChiusura.click(function () {
   var listaMenu = $('.hamburger-menu');
   //Diciamo alla lista contente il menù che deve nascondersi
   listaMenu.css({display:'none'});
-})
+});
+*/
+
+// --- SOLUZIONE 3 ---
+
+//Dichiaro la variabile e inserisco dentro il
+//collegamento all'a contente il simbolo del menù
+var hamburgerMenu = $('.header-right > a');
+
+//Al click sul hamburgerMenu viene eseguita questa funzione
+hamburgerMenu.click(function() {
+  //Richiamiamo la lista contente il menù e gli assegniamo una variabile
+  var listaMenu = $('.hamburger-menu');
+  //Diciamo alla lista contente il menù che deve farsi vedere
+  listaMenu.addClass('active');
+});
+
+//Dichiariamo la variabile hamburgerMenuChiusura che riprende il pulsante di chiusura
+var hamburgerMenuChiusura = $('.close');
+
+//Al click su hamburgerMenuChiusura la funzione nasconderà listaMenu
+hamburgerMenuChiusura.click(function () {
+  //Richiamiamo la lista contente il menù
+  var listaMenu = $('.hamburger-menu');
+  //Diciamo alla lista contente il menù che deve nascondersi
+  listaMenu.removeClass('active');
+});
